bb9b22b79e223ca06d6d9cafb17ad5e2dd09dea5
MP <matt@MPs-MacBook-Pro.local>
2020-06-19 10:41:04 -0700

    comp functions completed

--- functions_and_classes/comparisons.py
+++ functions_and_classes/comparisons.py
@@ -3,6 +3,14 @@ import json
 from data import csv_dir, json_dir
 from data.csvs import csv_list
 from data.jsons import json_list
+from data import a_l_p_comp, a_l_c_comp, s_l_p_comp, s_l_c_comp
+#This file has two categories of thigns worth importing for usage:
+#first is a function for generating comparisons:
+#article_comps(filename, min_num_cites=1, min_num_comps=100, a_or_s="a", p_or_c="p")
+#Second are the comparative data frame updaters:
+#updateALCcomps, updateALPcomps, updateSLCcomps, updateSLPcomps
+#those will update the a_l_p_comp csvs and when the data module is reloaded it will
+#draw the new data frames.
 
 def df_col_similarity(df, colx, coly):
     total_diff = 0
@@ -113,3 +121,65 @@ def updateALCcomps():
     a_l_c_comps = pd.merge(a_l_c_comps, agg_df, left_index=True, right_index=True)
     a_l_c_comps.to_csv('data/a_l_c_comp.csv')
     print('Done updating a_l_c_comp.csv')
+
+
+#now that we've finished the functions for generating and saving data I'll add functions for generating comparative data for an individual article
+
+def n_agg_filter(df_in, num):
+    return df_in['totalAggressiveCites'] > num
+
+def m_agg_similar(filename, min_cites, min_similar, df_in):
+    min_cit = max(0,min_cites)
+    min_c = n_agg_filter(df_in, min_cit)
+    min_si = min(min_similar,len(df_in[min_c].index)-1)
+    sim_score_to_beat = 1.05 * df_in[min_c].sort_values(filename)[min_si:min_si+1][filename].values[0]
+    score_to_beat_filter = df_in[min_c][filename] <= sim_score_to_beat
+
+    return df_in[min_c][score_to_beat_filter].sort_values(filename)
+
+def a_l_c_comp_process(file, minimum_comp_cites, minimum_sim_comps):
+    df = pd.merge(a_l_c_comp[file], a_l_c_comp['totalAggressiveCites'], left_index=True, right_index=True)
+    return m_agg_similar(file, minimum_comp_cites, minimum_sim_comps, df)[1:]
+
+def a_l_p_comp_process(file, minimum_comp_cites, minimum_sim_comps):
+    df = pd.merge(a_l_p_comp[file], a_l_p_comp['totalAggressiveCites'], left_index=True, right_index=True)
+    return m_agg_similar(file, minimum_comp_cites, minimum_sim_comps, df)[1:]
+
+def n_strict_filter(df_in, num):
+    return df_in['totalStrictCites'] > num
+
+def m_strict_similar(filename, min_cites, min_similar, df_in):
+    min_cit = max(0,min_cites)
+    min_c = n_strict_filter(df_in, min_cit)
+    min_si = min(min_similar,len(df_in[min_c].index)-1)
+    sim_score_to_beat = 1.05 * df_in[min_c].sort_values(filename)[min_si:min_si+1][filename].values[0]
+    score_to_beat_filter = df_in[min_c][filename] <= sim_score_to_beat
+
+    return df_in[min_c][score_to_beat_filter].sort_values(filename)
+
+def s_l_c_comp_process(file, minimum_comp_cites, minimum_sim_comps):
+    df = pd.merge(s_l_c_comp[file], s_l_c_comp['totalStrictCites'], left_index=True, right_index=True)
+    return m_strict_similar(file, minimum_comp_cites, minimum_sim_comps, df)[1:]
+
+def s_l_p_comp_process(file, minimum_comp_cites, minimum_sim_comps):
+    df = pd.merge(s_l_p_comp[file], s_l_p_comp['totalStrictCites'], left_index=True, right_index=True)
+    return m_strict_similar(file, minimum_comp_cites, minimum_sim_comps, df)[1:]
+
+#this is in the end all that needs to be imported from this file to generate the comparative data for an individaul file, along with the a_l_p_comp etc dfs from data
+def article_comps(filename, min_num_cites=1, min_num_comps=100, a_or_s="a", p_or_c="p"):
+    if a_or_s == "a":
+        if p_or_c == "p":
+            return a_l_p_comp_process(filename, minimum_comp_cites=min_num_cites, minimum_sim_comps=min_num_comps)
+        elif p_or_c == "c":
+            return a_l_c_comp_process(filename, minimum_comp_cites=min_num_cites, minimum_sim_comps=min_num_comps)
+        else:
+            return print('be sure to choose "p" or "c" for the p_or_c argument')
+    elif a_or_s == "s":
+        if p_or_c == "p":
+            return s_l_p_comp_process(filename, minimum_comp_cites=min_num_cites, minimum_sim_comps=min_num_comps)
+        elif p_or_c == "c":
+            return s_l_c_comp_process(filename, minimum_comp_cites=min_num_cites, minimum_sim_comps=min_num_comps)
+        else:
+            return print('be sure to choose "p" or "c" for the p_or_c argument')
+    else:
+        return print('be sure to choose "a" or "s" for the a_or_s argument')
--- functions_and_classes/lit_level_operations.py
+++ functions_and_classes/lit_level_operations.py
@@ -10,47 +10,6 @@ from functions_and_classes.paper_processing import process_and_score_paper as ps
 from functions_and_classes.paper_processing import chapterScores, relativeScoreCalc
 from functions_and_classes.paper_io import p_from_dict, p_to_dict, c_from_dict, c_to_dict
 
-#generate the literature level scores from scratch by processing text files
-def lit_scores_from_txts():
-    #create the list of paragraphs to serve as the score sheet
-    master_strict_para_list = {}
-    for para in treatise_paragraph_list:
-        master_strict_para_list[para] = 0
-    #create the list of chapter to serve as the c
-    master_strict_chap_list = chapterScores(master_strict_para_list)
-
-    master_agg_para_list = {}
-    for para in treatise_paragraph_list:
-        master_agg_para_list[para] = 0
-    #create the list of chapter to serve as the c
-    master_agg_chap_list = chapterScores(master_agg_para_list)
-
-    #now loop over the text files:
-    for txt_file in txt_list:
-        paper = psp(txt_file[:-4])
-        for para in master_strict_para_list.keys():
-                master_strict_para_list[para] += paper.s_w_p[para]
-        for chapter in master_strict_chap_list.keys():
-                master_strict_chap_list[chapter] += paper.s_w_c[chapter]
-        if paper.totalStrictCites < 1:
-                print(txt_file, 'has no strict cites')
-        for para in master_agg_para_list.keys():
-                master_agg_para_list[para] += paper.a_w_p[para]
-        for chapter in master_agg_chap_list.keys():
-                master_strict_chap_list[chapter] += paper.a_w_c[chapter]
-        if paper.totalAggressiveCites < 1:
-                print(txt_file, 'has no aggressive cites')
-
-    lit_s_l_p = relativeScoreCalc(master_strict_para_list)
-    lit_s_l_c = chapterScores(lit_s_l_p)
-    lit_a_l_p = relativeScoreCalc(master_agg_para_list)
-    lit_a_l_c = chapterScores(lit_a_l_p)
-
-    od = {'lit_s_w_p': master_strict_para_list, 'lit_s_w_c': master_strict_chap_list, 'lit_a_w_p': master_agg_para_list, 'lit_a_w_c': master_agg_chap_list, 'lit_s_l_p': lit_s_l_p, 'lit_s_l_c': lit_s_l_c, 'lit_a_l_p': lit_a_l_p, 'lit_a_l_c': lit_a_l_c}
-
-    return od
-
-
 #generate lit level scores for all 8 kinds by reading from stored JSON data
 def lit_scores_from_jsons():
     #create the list of paragraphs to serve as the score sheet
